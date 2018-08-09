f4de564ca383e35a688152c802d3bc63a62bb549
Babak Niroomand <babakniroomand@hotmail.com>
2018-08-09 14:04:12 +0430

    Add project files.

---
+++ BobThreadPool/ThreadPool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BobThreadPool
+{
+    public class ThreadPool
+    {
+        private Queue<Task> tasks;
+        private Queue<Task> runningTasks;
+        private List<Thread> threads;
+
+        public ThreadPool(int size = 10)
+        {
+            tasks = new Queue<Task>();
+            threads = new List<Thread>();
+            //in vase teste
+            for (int i = 0; i < size; i++)
+            {
+                Thread newThread = new Thread(WaitForTask);
+                threads.Add(newThread);
+                newThread.Start();
+            }
+        }
+
+        private void WaitForTask()
+        {
+            //TODO wait for task queue to get item from it
+            while (true)
+            {
+                lock (tasks)
+                {
+                    if (tasks.Count != 0)
+                    {
+                        //TODO do the task's job!
+                        Task t = tasks.Dequeue();
+                        t.Start();
+                    }
+                }
+            }
+        }
+    }
+}
