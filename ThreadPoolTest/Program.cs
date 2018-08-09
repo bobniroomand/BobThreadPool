bcf124bc49c1f606e7d2bcc9a1773c287e3d133a
Babak Niroomand <babakniroomand@hotmail.com>
2018-08-09 15:46:49 +0430

    wrote a test for watching the impact of using thread pool

--- ThreadPoolTest/Program.cs
+++ ThreadPoolTest/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace ThreadPoolTest
 {
@@ -10,6 +9,43 @@ namespace ThreadPoolTest
     {
         static void Main(string[] args)
         {
+            Stopwatch myWatch = new Stopwatch();
+            myWatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                PrintWithThread(i);
+            }
+            myWatch.Stop();
+            Console.WriteLine("Elapsed Time without threadpooling: {0}", myWatch.ElapsedTicks);
+
+            myWatch.Reset();
+
+            BobThreadPool.ThreadPool tp = new BobThreadPool.ThreadPool();
+            myWatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                PrintWithThreadPool(tp, i);
+            }
+            myWatch.Stop();
+            Console.WriteLine("Elapsed Time with threadpooling: {0}", myWatch.ElapsedTicks);
+        }
+
+        static void PrintWithThread(int i)
+        {
+            Thread t = new Thread(() => { Print(i); });
+            t.Start();
+        }
+
+        static void PrintWithThreadPool(BobThreadPool.ThreadPool tp, int i)
+        {
+            Action<object> a = o => Print((int)o);
+            Task t = new Task(a, i);
+            tp.EnqueueTask(t);
+        }
+
+        static void Print(int i)
+        {
+            Console.WriteLine("this is task number {0}", i);
         }
     }
 }
