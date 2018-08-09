50358402accc9ac20662d98726f8c685d36b9439
Babak Niroomand <babakniroomand@hotmail.com>
2018-08-09 19:27:54 +0430

    implemented Job class, it will be used instead of Task in ThreadPool;

---
+++ BobThreadPool/Job.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobThreadPool
+{
+    class Job
+    {
+        Action task;
+        public JobState State { get; private set; }
+
+        Job(Action a)
+        {
+            task = a;
+            State = JobState.Waiting;
+        }
+
+        void Run()
+        {
+            State = JobState.Working;
+            task();
+            State = JobState.Terminated;
+        }
+
+    }
+
+    enum JobState
+    {
+        Waiting,
+        Working,
+        Terminated
+    }
+}
